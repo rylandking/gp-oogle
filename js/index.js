559b05eb3298078b807bd66b10019169ca111db6
rylandking <ryland@users.noreply.github.com>
2020-01-18 08:31:36 -0700

    Showing user email to logged in user

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
+const userEmail = document.querySelector('#user-email');
 
 const setupUI = user => {
   if (user) {
     // Toggle UI Elements
     loggedInLinks.forEach(item => (item.style.display = 'block'));
     loggedOutLinks.forEach(item => (item.style.display = 'none'));
+
+    // Show user email
+    userEmail.append(user.email);
   } else {
     // Toggle UI Elements
     loggedInLinks.forEach(item => (item.style.display = 'none'));
     loggedOutLinks.forEach(item => (item.style.display = 'block'));
+
+    // Reset user email
+    userEmail.innerHTML = '';
   }
 };
 
